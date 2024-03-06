e65ac6e5c7b5735bc4a51d030d7af394ca4cd5f4
Jamal Aw yonis <jamalawyonis@Jamals-MacBook-Pro.local>
2024-03-05 19:00:25 -0800

    updated

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+ 
+export const inter = Inter({ subsets: ['latin'] });
+// export const lusitana  fonts 
+export const lusitana = Lusitana({subsets:['latin'], weight:['400', '700']})
